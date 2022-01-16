722c5ee3a4d3d3b455e1a5b060a7a064782298d5
Dima <pandora-house@mail.ru>
2022-01-17 01:57:26 +0300

    add custom painter

--- lib/widgets/box_container_widget.dart
+++ lib/widgets/box_container_widget.dart
@@ -10,18 +10,63 @@ class BoxContainerWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    const padding = 0.5;
+    const padding = 2.0;
     return Padding(
       padding: const EdgeInsets.all(padding),
-      child: Container(
+      child: SizedBox(
         width: Engine().getBoxWith(size) - 2 * padding,
         height: Engine().getBoxHeight(size) - 2 * padding,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(7),
-          color: Colors.grey,
+        // decoration: BoxDecoration(
+        //   borderRadius: BorderRadius.circular(7),
+        //   color: Colors.grey,
+        // ),
+        // child: Center(child: Text('$id')),
+        child: CustomPaint(
+          painter: MyPainter(),
         ),
-        child: Center(child: Text('$id')),
       ),
     );
+  }
+}
+
+class MyPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    double width = size.width;
+    double height = size.height;
+    double radius = 6;
+
+    Paint blackPaint = Paint()
+      ..color = Colors.black
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 3;
+
+    Paint redPaint = Paint()
+      ..color = Colors.red
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 3;
+
+    RRect fullRect = RRect.fromRectAndRadius(
+      Rect.fromCenter(
+          center: Offset(width / 2, height / 2), width: width, height: height),
+      Radius.circular(radius),
+    );
+
+    Path topRightArc = Path()
+      ..moveTo(width - radius, 0)
+      ..arcToPoint(Offset(width, radius), radius: Radius.circular(radius));
+
+    Path bottomLeftArc = Path()
+      ..moveTo(radius, height)
+      ..arcToPoint(Offset(0, height - radius), radius: Radius.circular(radius));
+
+    canvas.drawRRect(fullRect, blackPaint);
+    canvas.drawPath(topRightArc, redPaint);
+    canvas.drawPath(bottomLeftArc, redPaint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
   }
 }
