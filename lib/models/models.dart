499ff750e9e7125e2b5ce9b57249051b3781a6a3
Dima <pandora-house@mail.ru>
2022-01-17 00:00:35 +0300

    add models

---
+++ lib/models/models.dart
@@ -0,0 +1,13 @@
+class PositionBox {
+  final double x;
+  final double y;
+
+  PositionBox({required this.x, required this.y});
+}
+
+class IdIndex {
+  final int x;
+  final int y;
+
+  IdIndex({required this.x, required this.y});
+}
