cf4a8a5bdfd3c71e3ca9378c6faa730aaa626168
Dima <pandora-house@mail.ru>
2022-01-24 00:11:32 +0300

    add paths

---
+++ lib/paths.dart
@@ -0,0 +1,40 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+import 'engine.dart';
+import 'home_page.dart';
+import 'widgets/ball_container_widget.dart';
+import 'widgets/box_container_widget.dart';
+
+class Paths {
+  static Path drawBallPath(Size size) {
+    const padding = PuzzlePage.padding;
+    const internalGap = PuzzlePage.internalGap;
+    final boxWith = Engine().getBoxWith(size);
+    final boxHeight = Engine().getBoxHeight(size);
+    const boxBorderWidth = BoxContainerWidget.borderWidth;
+    const ballSize = BallContainerWidget.size;
+    final quantityGaps = Engine().quantityInternalGaps();
+
+    final initX = boxWith / 2 +
+        padding / 2 -
+        ballSize / 2 +
+        internalGap * quantityGaps +
+        boxBorderWidth / 2;
+
+    final initY = boxHeight / 2 +
+        padding / 2 -
+        ballSize / 2 +
+        internalGap * quantityGaps +
+        boxBorderWidth / 2;
+
+    final path = Path()
+      ..moveTo(initX, initY)
+      ..lineTo(initX, initY + boxHeight * 2)
+      ..lineTo(initX + boxWith, initY + boxHeight * 2)
+      ..lineTo(initX + boxWith, initY + boxHeight * 3)
+      ..lineTo(initX + boxWith * 3, initY + boxHeight * 3);
+    return path;
+  }
+}
--- lib/widgets/ball_animated_widget.dart
+++ lib/widgets/ball_animated_widget.dart
@@ -1,11 +1,58 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+
+import '../paths.dart';
 import 'ball_container_widget.dart';
 
-class BallAnimatedWidget extends StatelessWidget {
-  const BallAnimatedWidget({Key? key}) : super(key: key);
+class BallAnimatedWidget extends StatefulWidget {
+  const BallAnimatedWidget({Key? key, required this.size}) : super(key: key);
+
+  final Size size;
+
+  @override
+  State<BallAnimatedWidget> createState() => _BallAnimatedWidgetState();
+}
+
+class _BallAnimatedWidgetState extends State<BallAnimatedWidget>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _controller;
+  late final Animation _animation;
+  late final Path _path;
+
+  @override
+  void initState() {
+    _controller =
+        AnimationController(vsync: this, duration: const Duration(seconds: 6));
+    super.initState();
+    _animation = Tween(begin: 0.0, end: 1.0).animate(_controller)
+      ..addListener(() {
+        // todo change update ui
+        setState(() {});
+      });
+    _controller.forward();
+    _path = Paths.drawBallPath(widget.size);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Positioned(top: 100, left: 100, child: BallContainerWidget());
+    return Positioned(
+        top: calculate(_animation.value).dy,
+        left: calculate(_animation.value).dx,
+        child: const BallContainerWidget());
+  }
+
+  Offset calculate(value) {
+    PathMetrics pathMetrics = _path.computeMetrics();
+    PathMetric pathMetric = pathMetrics.elementAt(0);
+    value = pathMetric.length * value;
+    Tangent? pos = pathMetric.getTangentForOffset(value);
+    return pos!.position;
   }
 }
